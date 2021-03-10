43ed87d50a40bf9161947f7de1c2046e782345de
unknown <kaja.ricna@seznam.cz>
2021-03-10 16:54:39 +0100

    Druhá část tutoriálu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const socket = require('socket.io');
 
 const app = express();
 const server = app.listen(4000, function(){
@@ -7,3 +7,10 @@ console.log('Server is running on port 4000');
 });
 
 app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
+
+});
