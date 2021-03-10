4e9b79fd8803d13d8bf6d72cbee7ba244b2972b9
unknown <kaja.ricna@seznam.cz>
2021-03-10 18:14:26 +0100

    Vlastní drobné úpravy

--- public/chat.js
+++ public/chat.js
@@ -1,27 +1,28 @@
-const socket = io.connect('http://localhost:4000');
+const socket = io();
 
-let message = document.getElementById('message');
-let handle = document.getElementById('handle');
-let btn = document.getElementById('send');
-let output = document.getElementById('output');
-let feedback = document.getElementById('feedback');
+let message = document.getElementById("message");
+let handle = document.getElementById("handle");
+let btn = document.getElementById("send");
+let output = document.getElementById("output");
+let feedback = document.getElementById("feedback");
 
-btn.addEventListener('click', function(){
-    socket.emit('chat', {
-        message: message.value,
-        handle: handle.value
-    });
+btn.addEventListener("click", () => {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value,
+  });
 });
 
-message.addEventListener('keypress', function() {
-    socket.emit('typing', handle.value);
+message.addEventListener("keypress", () => {
+  socket.emit("typing", handle.value);
 });
 
-socket.on('chat', function(data){
-    feedback.innerHTML ="";
-output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+socket.on("chat", (data) => {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
 });
 
-socket.on('typing', function(data){
-    feedback.innerHTML = '<p><em>'+ data + ' is typing a message...</em></p>';
-    });
+socket.on("typing", (data) => {
+  feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
+});
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const socket = require("socket.io");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
+});
+
+const PORT = 4000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
