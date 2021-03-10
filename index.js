7d9ef89f193bdb51c333b4b0d7e45b0437f0613a
unknown <kaja.ricna@seznam.cz>
2021-03-10 17:19:45 +0100

    Třetí část tutoriálu

--- index.js
+++ index.js
@@ -13,4 +13,10 @@ const io = socket(server);
 io.on('connection', function(socket){
     console.log('made socket connection', socket.id);
 
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+
+
+    
 });
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,17 @@
 const socket = io.connect('http://localhost:4000');
+
+let message = document.getElementById('message');
+let handle = document.getElementById('handle');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+});
+
+socket.on('chat', function(data){
+output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
