850a24a1c0217a34eb5305760562366dfe3bd26c
unknown <kaja.ricna@seznam.cz>
2021-03-21 09:51:43 +0100

    Úpravy designu + přihlášení

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,5 @@
 const socket = io();
+//const time = timeInfo();
 
 let message = document.getElementById("message");
 let handle = document.getElementById("handle");
@@ -6,11 +7,15 @@ let btn = document.getElementById("send");
 let output = document.getElementById("output");
 let feedback = document.getElementById("feedback");
 let chatMessages = document.getElementById("chat-window");
+let login = document.getElementById('login');
+let loginBlock = document.getElementById('loginBlock');
+let chatBlock = document.getElementById('chatBlock');
 
 btn.addEventListener("click", () => {
   socket.emit("chat", {
     message: message.value,
     handle: handle.value,
+    // time: time.value,
   });
   message.value = '';
   message.focus();
@@ -23,10 +28,15 @@ message.addEventListener("keypress", () => {
 socket.on("chat", (data) => {
   feedback.innerHTML = "";
   output.innerHTML +=
-    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+    "<p><strong>" + data.handle + ": </strong>" /*+ data.time + "<br>"*/+ data.message + "</p>";
     chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 socket.on("typing", (data) => {
   feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
+});
+
+login.addEventListener('click', () => {
+  loginBlock.style.display = 'none';
+  chatBlock.style.display = '';
 });
